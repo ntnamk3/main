01e0a964ba9ed8b1294cd7f3368c29b77dd8a4f8
Thế Nam <nthe0241@gmail.com>
2021-10-05 23:54:23 +0700

    Update 040903.php

--- js/nam/040903.php
+++ js/nam/040903.php
@@ -4,7 +4,7 @@
 $user = $_GET['api'];
 
 if($user){
-// lượng thời gian đăng ký của người dùng đang hoạt động, id người dùng có bị chặn không? 0 = No 1 = TRUE, chỉ cần người dùng đặt tên cho nó là không có gì
+
     function subscriptionManager($setDate, $setBlacklisted, $userName) {
     	$date = new DateTime("$setDate 00:00:00");
     	$date->modify("1 day");
@@ -19,13 +19,11 @@ if($user){
         return $finishedString;
     }
 
-    //this should be device unique id '100' change that for every user make a if statement for each user and add a date etc
+ 
     if($user == '100'){
         echo subscriptionManager("2022-03-08", "1", "iosnab");
     }
-    //note 200 will be user identifier id. User will copy id when Modmenu appears and they will show it to you, you put it here 
-
-    //to block the user device set 0 to 1.
+   
     if($user == '200'){
         echo subscriptionManager("2023-03-08", "1", "iosnab");
     }
