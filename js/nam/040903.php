51eef12cbb391233654cdc11d24254f5851f5364
Thế Nam <nthe0241@gmail.com>
2021-10-05 23:51:23 +0700

    Update 040903.php

--- js/nam/040903.php
+++ js/nam/040903.php
@@ -7,7 +7,7 @@ if($user){
 // lượng thời gian đăng ký của người dùng đang hoạt động, id người dùng có bị chặn không? 0 = No 1 = TRUE, chỉ cần người dùng đặt tên cho nó là không có gì
     function subscriptionManager($setDate, $setBlacklisted, $userName) {
     	$date = new DateTime("$setDate 00:00:00");
-    	$date->modify("-1 day");
+    	$date->modify("1 day");
 
     	$datenum=strtotime($date->format("Y-m-d H:i:s"));
     	$diff=$datenum-time();
@@ -21,7 +21,7 @@ if($user){
 
     //this should be device unique id '100' change that for every user make a if statement for each user and add a date etc
     if($user == '100'){
-        echo subscriptionManager("2022-03-08", "0", "iosnab");
+        echo subscriptionManager("2022-03-08", "1", "iosnab");
     }
     //note 200 will be user identifier id. User will copy id when Modmenu appears and they will show it to you, you put it here 
 
